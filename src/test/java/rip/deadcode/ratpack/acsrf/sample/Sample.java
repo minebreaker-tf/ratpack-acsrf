b5d13c336bb215e4a31b3949ea561c2547eb2d25
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2018-02-10 19:58:22 +0900

    updated sample

--- src/test/java/rip/deadcode/ratpack/acsrf/sample/Sample.java
+++ src/test/java/rip/deadcode/ratpack/acsrf/sample/Sample.java
@@ -5,6 +5,8 @@ import ratpack.server.RatpackServer;
 import ratpack.session.SessionModule;
 import rip.deadcode.ratpack.acsrf.AntiCsrfModule;
 import rip.deadcode.ratpack.acsrf.CsrfHandler;
+import rip.deadcode.ratpack.acsrf.CsrfTokenManager;
+import rip.deadcode.ratpack.acsrf.RandomCsrfTokenManager;
 
 public final class Sample {
 
@@ -13,7 +15,9 @@ public final class Sample {
         RatpackServer.start( spec -> {
             spec.registry( Guice.registry( bindings -> {
                 bindings.module( SessionModule.class )
-                        .module( AntiCsrfModule.class );
+                        .module( AntiCsrfModule.class )
+                        .bind( CsrfTokenManager.class, RandomCsrfTokenManager.class )
+                ;
             } ) )
                 .serverConfig( config -> config.findBaseDir( "rip/deadcode/ratpack/acsrf/sample/.ratpack" ) )
                 .handlers( chain -> {
